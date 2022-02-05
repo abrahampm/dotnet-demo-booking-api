0ef80e82320e6c8ced51d1ebc1af5a6418e254af
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2022-02-04 22:12:11 -0500

    Create Reservation Repository in Data Access Layer

---
+++ alten-test.DataAccessLayer/Repositories/ReservationRepository.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+using alten_test.Core.Models;
+using alten_test.Core.Interfaces;
+using alten_test.DataAccessLayer.Context;
+using alten_test.DataAccessLayer.Interfaces;
+using alten_test.DataAccessLayer.Extensions;
+
+
+namespace alten_test.DataAccessLayer.Repositories
+{
+    public class ReservationRepository: Repository<Reservation>, IReservationRepository
+    {
+        public ReservationRepository(DatabaseContext context) : base(context) { }
+
+
+        public new IQueryable<Reservation> GetAll()
+        {
+            return (from e in _entities select e).Include("Contact").Include("Room");
+        }
+
+        public new async Task<Reservation> GetById(int id)
+        {
+            return await _entities.Include(r => r.Contact).Include(r => r.Room)
+                .FirstOrDefaultAsync(r => r.Id == id);
+        }
+    }
+}
