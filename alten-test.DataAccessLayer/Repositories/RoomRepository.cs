add4c3222be64311d472c3f046ea629b12ff37d5
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2022-02-04 22:11:52 -0500

    Create Room Repository in Data Access Layer

---
+++ alten-test.DataAccessLayer/Repositories/RoomRepository.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+using alten_test.Core.Models;
+using alten_test.Core.Interfaces;
+using alten_test.Core.Utilities;
+using alten_test.DataAccessLayer.Context;
+using alten_test.DataAccessLayer.Interfaces;
+using alten_test.DataAccessLayer.Extensions;
+
+
+namespace alten_test.DataAccessLayer.Repositories
+{
+    public class RoomRepository: Repository<Room>, IRoomRepository
+    {
+        public RoomRepository(DatabaseContext context) : base(context) { }
+
+        public async Task<List<Room>> GetAvailableWithStoredProcedure(DateTime startDate, DateTime endDate, RoomType roomType)
+        {
+            return await _entities.FromSqlInterpolated($"CALL `GetAvailableRooms`({startDate},{endDate},{(int) roomType})").ToListAsync();
+        }
+    }
+}
