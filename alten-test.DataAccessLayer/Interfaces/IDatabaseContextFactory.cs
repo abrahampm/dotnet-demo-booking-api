bcd35ed8e4cb168e85b30fb8df08be80370bfeb7
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2022-02-06 02:13:01 -0500

    Remove Database Context Factory in Data Access Layer

--- alten-test.DataAccessLayer/Interfaces/IDatabaseContextFactory.cs
+++
@@ -1,9 +0,0 @@
-using alten_test.DataAccessLayer.Context;
-
-namespace alten_test.DataAccessLayer.Interfaces
-{
-    public interface IDatabaseContextFactory
-    {
-        ApplicationDbContext GetContext();
-    }
-}
