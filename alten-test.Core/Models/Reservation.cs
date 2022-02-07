445d7bf7d79d48f4f8805f816638739e1daa9431
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2022-02-07 13:34:45 -0500

    Update Reservation Model Foreign Keys

--- alten-test.Core/Models/Reservation.cs
+++ alten-test.Core/Models/Reservation.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using alten_test.Core.Models.Authentication;
 
 namespace alten_test.Core.Models
@@ -13,16 +14,18 @@ namespace alten_test.Core.Models
         public DateTime EndDate { get; set; }
         
         [Required]
+        [ForeignKey("ApplicationUser")]
         public string ApplicationUserId { get; set; }
 
         [Required]
-        public ApplicationUser ApplicationUser { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
 
         [Required]
+        [ForeignKey("Room")]
         public int RoomId { get; set; }
         
         [Required]
-        public Room Room { get; set; }
+        public virtual Room Room { get; set; }
 
         public string Description { get; set; }
 
