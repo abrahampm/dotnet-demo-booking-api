be42ba3e118d78886accf9402766d2d3a25fb9cf
Carlos Abraham Pérez Marrero <abrahampm96@gmail.com>
2022-02-08 11:13:24 -0500

    Add Prodcution Update Fields Max Length Migration

---
+++ alten-test.DataAccessLayer/Migrations/20220208155851_ProductionUpdateFieldsMaxLength.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace alten_test.DataAccessLayer.Migrations
+{
+    public partial class ProductionUpdateFieldsMaxLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
